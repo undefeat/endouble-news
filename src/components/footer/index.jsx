bb19bc60315362be2d825b10718467d995ca0eb6
Dmytro Kostiuk <dmytro.kostiuk@binary-studio.com>
2019-02-08 14:11:35 +0200

    Add newsapi attribution link to the footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Footer() {
-    return <footer>Footer</footer>;
+    return (
+        <footer>
+            <span>Powered by </span>
+            <a href="https://newsapi.org" rel="noopener noreferrer" target="_blank">
+                NewsAPI.org
+            </a>
+        </footer>
+    );
 }
 
 export default Footer;
